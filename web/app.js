280d17840df5b9b89785400fd94a7086f1b83fbc
Jeff Shillitto <jeff@shotstack.io>
2022-05-08 22:36:20 +1000

    Move URL comment to same line

--- web/app.js
+++ web/app.js
@@ -1,5 +1,4 @@
-// var apiUrl = 'https://vanhnt5aid.execute-api.ap-southeast-2.amazonaws.com/demo/';
-var apiUrl = 'http://localhost:3000/demo/';
+var apiUrl = 'http://localhost:3000/demo/'; // 'https://vanhnt5aid.execute-api.ap-southeast-2.amazonaws.com/demo/';
 var apiEndpoint = apiUrl + 'shotstack';
 var urlEndpoint = apiUrl + 'upload/sign';
 var probeEndpoint = 'https://api.shotstack.io/stage/probe/';
